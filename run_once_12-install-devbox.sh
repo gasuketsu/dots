21420748ac857f6d6390963dc70b60335adf933f
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2026-06-22 09:53:27 +0900

    chore(script): install devbox from nixpkgs

--- run_once_12-install-devbox.sh
+++ run_once_12-install-devbox.sh
@@ -11,4 +11,4 @@ if [ -z "${__ETC_PROFILE_NIX_SOURCED}" ]; then
     . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
-nix profile add github:jetify-com/devbox/latest
+nix profile add "nixpkgs#devbox"

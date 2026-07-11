d89b6d244643c5b8d4bd9e4a9d30ce43df90be29
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2026-07-11 10:01:45 +0900

    feat(neovim): normalize path in special buffers for dropbar

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -42,6 +42,8 @@ return {
             local get_color = require("lualine.utils.utils").extract_highlight_colors
 
             require("lualine").setup({
+                winbar = nil,
+                inactive_winbar = nil,
                 sections = {
                     lualine_b = {
                         { "branch" },
@@ -161,5 +163,58 @@ return {
     -- winbar
     {
         "Bekaboo/dropbar.nvim",
+        config = function()
+            require("dropbar").setup({
+                bar = {
+                    enable = function(buf, win, _)
+                        buf = vim._resolve_bufnr(buf)
+                        if not vim.api.nvim_buf_is_valid(buf) or not vim.api.nvim_win_is_valid(win) then
+                            return false
+                        end
+
+                        if
+                            not vim.api.nvim_buf_is_valid(buf)
+                            or not vim.api.nvim_win_is_valid(win)
+                            or vim.fn.win_gettype(win) ~= ""
+                            or vim.wo[win].winbar ~= ""
+                            or vim.bo[buf].ft == "help"
+                        then
+                            return false
+                        end
+
+                        local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+                        if stat and stat.size > 1024 * 1024 then
+                            return false
+                        end
+
+                        return vim.bo[buf].bt == "terminal"
+                            or vim.bo[buf].ft == "markdown"
+                            or pcall(vim.treesitter.get_parser, buf)
+                            or not vim.tbl_isempty(vim.lsp.get_clients({
+                                bufnr = buf,
+                                method = "textDocument/documentSymbol",
+                            }))
+                    end,
+                },
+                sources = {
+                    path = {
+                        relative_to = function(buf, win)
+                            -- Show full path in oil or fugitive buffers
+                            local bufname = vim.api.nvim_buf_get_name(buf)
+                            if vim.startswith(bufname, "oil://") or vim.startswith(bufname, "fugitive://") then
+                                local root = bufname:gsub("^%S+://", "", 1)
+                                while root and root ~= vim.fs.dirname(root) do
+                                    root = vim.fs.dirname(root)
+                                end
+                                return root
+                            end
+
+                            local ok, cwd = pcall(vim.fn.getcwd, win)
+                            return ok and cwd or vim.fn.getcwd()
+                        end,
+                    },
+                },
+            })
+        end,
     },
 }

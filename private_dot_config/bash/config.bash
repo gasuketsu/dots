81e80c28ceaab5c47d105522e25df3bcb05939e2
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2026-09-09 00:01:18 +0900

    fix(shell): herdr completion does not work

--- private_dot_config/bash/config.bash
+++ private_dot_config/bash/config.bash
@@ -13,4 +13,4 @@ if ! echo $PATH | grep -q "$HOME/.local/bin"; then
 fi
 
 eval "$(zoxide init bash)"
-eval "$(herdr completion zsh)"
+eval "$(herdr completion bash)"
--- private_dot_config/zsh/config.zsh
+++ private_dot_config/zsh/config.zsh
@@ -15,7 +15,6 @@ fi
 
 eval "$(sheldon source)"
 eval "$(zoxide init zsh)"
-eval "$(herdr completion zsh)"
 
 if type fzf >/dev/null 2>&1; then
     source <(fzf --zsh)
